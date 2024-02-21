1be3389ca516272a56944a06d1f2e132c8fc3067
jose <jose.manuel.cardoso@iessanmamede.com>
2024-02-21 15:18:54 +0100

    Inicio portafolio

---
+++ src/main/java/ad/apiud6josemanuel/ApiUd6JoseManuelApplication.java
@@ -0,0 +1,13 @@
+package ad.apiud6josemanuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUd6JoseManuelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiUd6JoseManuelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ad/apiud6josemanuel/ApiUd6JoseManuelApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.apiud6josemanuel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUd6JoseManuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
