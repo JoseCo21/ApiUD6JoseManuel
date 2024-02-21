4236e4119233b81b66aae1d19ea91ba2f53430f4
jose <jose.manuel.cardoso@iessanmamede.com>
2024-02-21 15:59:53 +0100

    Creacion controladores

--- src/main/java/ad/apiud6josemanuel/controlador/JuegoControlador.java
+++ src/main/java/ad/apiud6josemanuel/controlador/JuegoControlador.java
@@ -1,4 +1,56 @@
 package ad.apiud6josemanuel.controlador;
 
+import ad.apiud6josemanuel.modelo.Juego;
+import ad.apiud6josemanuel.repositorio.JuegoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/juego")
 public class JuegoControlador {
+    @Autowired
+    private JuegoRepositorio juegoRepositorio;
+
+    //todas las categorias
+    @GetMapping
+    public List<Juego> obtenerCategorias(){
+        return juegoRepositorio.findAll();
+    }
+
+    //Categoria por id
+    @GetMapping("/{id}")
+    public Juego obtenerJuegoId(@PathVariable Long id){
+        Optional<Juego> resultado = juegoRepositorio.findById(id);
+        return resultado.orElseThrow(() -> new RuntimeException("Juego no encontrado"));
+
+    }
+
+    //Crear categoria
+    @PostMapping
+    public Juego crearJuego(@RequestBody Juego juego){
+        return juegoRepositorio.save(juego);
+    }
+
+    //Actualizar categoria
+    @PutMapping("/{id}")
+    public Juego actualizarJuego(@PathVariable Long id, @RequestBody Juego juego){
+        return juegoRepositorio.findById(id)
+                .map(juegoTemp -> {
+                    juegoTemp.setNombre(juego.getNombre());
+                    juegoTemp.setPlataforma(juego.getPlataforma());
+                    return juegoRepositorio.save(juegoTemp);
+                }).orElseThrow(() -> new RuntimeException("Categoria no encontrada"));
+    }
+
+    //Borrar categoria
+    @DeleteMapping("/{id}")
+    public void borrarJuego(@PathVariable Long id){
+        juegoRepositorio.deleteById(id);
+
+    }
+
 }
+
--- src/main/java/ad/apiud6josemanuel/controlador/PuntuacionControlador.java
+++ src/main/java/ad/apiud6josemanuel/controlador/PuntuacionControlador.java
@@ -1,4 +1,65 @@
 package ad.apiud6josemanuel.controlador;
 
+import ad.apiud6josemanuel.modelo.Puntuacion;
+import ad.apiud6josemanuel.repositorio.JuegoRepositorio;
+import ad.apiud6josemanuel.repositorio.PuntuacionRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/puntuacion")
 public class PuntuacionControlador {
+    @Autowired
+    private PuntuacionRepositorio puntuacionRepositorio;
+
+    @Autowired
+    private JuegoRepositorio juegoRepositorio;
+
+    //Todos los recursos
+    @GetMapping
+    public List<Puntuacion> obtenerPuntuaciones(){
+        return puntuacionRepositorio.findAll(Sort.by(Sort.Direction.DESC,"puntos"));
+    }
+
+    //Recurso por id
+    @GetMapping("/{id}")
+    public Puntuacion obtenerPuntuacionId(@PathVariable Long id){
+        Optional<Puntuacion> resultado = puntuacionRepositorio.findById(id);
+        return resultado.orElseThrow(() -> new RuntimeException("Puntuacion no encontrada"));
+
+    }
+
+    //Alta de un recurso
+    @PostMapping("/juego/{id}")
+    public Puntuacion crearPuntuacion(@PathVariable Long id, @RequestBody Puntuacion puntuacion){
+        Puntuacion pun= juegoRepositorio.findById(id).map(
+                juego -> {
+                    puntuacion.setJuego(juego);
+                    return puntuacionRepositorio.save(puntuacion);
+                }
+        ).orElseThrow(() -> new RuntimeException("Juego no encontrado"));
+        return pun;
+    }
+
+    //Actualizar recurso
+    @PutMapping("/{id}")
+    public Puntuacion actualizarPuntuacion(@PathVariable Long id, @RequestBody Puntuacion puntuacion){
+        return puntuacionRepositorio.findById(id)
+                .map(puntuacionTemp -> {
+                    puntuacionTemp.setNombre(puntuacion.getNombre());
+                    puntuacionTemp.setPuntos(puntuacion.getPuntos());
+                    return puntuacionRepositorio.save(puntuacionTemp);
+                }).orElseThrow(() -> new RuntimeException("Recurso no encontrado"));
+    }
+
+    //Borrar recurso
+    @DeleteMapping("/{id}")
+    public void borrarPuntuacion(@PathVariable Long id){
+        puntuacionRepositorio.deleteById(id);
+
+    }
 }
